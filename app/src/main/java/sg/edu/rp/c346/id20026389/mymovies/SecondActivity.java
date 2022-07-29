12f291e9049dbd74eeebd1d03059b680383eeb09
AfiqRP <21019423@rp.edu.sg>
2022-07-29 14:38:24 +0800

    tye

--- app/src/main/java/sg/edu/rp/c346/id20026389/mymovies/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20026389/mymovies/SecondActivity.java
@@ -3,12 +3,25 @@ package sg.edu.rp.c346.id20026389.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
+
+    ListView lvMovie;
+    ArrayList<Movie>alMovieList;
+    CustomAdapter caMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        lvMovie = findViewById(R.id.listViewShowMovies);
+        alMovieList = new ArrayList<>();
+
+
     }
 }
