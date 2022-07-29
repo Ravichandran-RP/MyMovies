9eff8a5f3da2d106f90a108ba685ff4f5904e5c2
AfiqRP <21019423@rp.edu.sg>
2022-07-29 12:25:26 +0800

    tye

---
+++ app/src/main/java/sg/edu/rp/c346/id20026389/mymovies/SecondActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id20026389.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
